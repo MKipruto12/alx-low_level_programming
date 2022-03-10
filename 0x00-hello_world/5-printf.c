d8f729550d4cad4cf71559e9301d0d4c397b1e1d
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 11:34:31 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- starts execution and termination of prog
+ * Return: 0 if executed succesfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
