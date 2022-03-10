30f69c32b7b4f3d0e38a24af81ca0206decb6f58
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 10:37:09 -0800

    c program outputting given info

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* library header */
+int main() {
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+	/*puts used in place of printf to output Programming is like building a multilingual puzzle
+	 *\n moves to next line*/
+}
