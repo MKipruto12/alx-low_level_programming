28f9ada325958b614e508c89a08a39c05934a23b
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 10:48:57 -0800

    chars reduced

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /* library header */
 int main(void) {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	/*puts used in place of printf to output Programming is like building a multilingual puzzle
-	 *\n moves to next line
+	/*puts used in place of printf
+	 * then moves to next line
 	 */
 }
