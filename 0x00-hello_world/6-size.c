63fe63751db858b2fd7b3142196df04b043bf7ee
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 11:54:31 -0800

    white space deletion

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main- outputs code block phrase
- * Reyurn: 0 if succesfully exucted
+ * Return: 0 if succesfully executed
  */
 int main(void)
 {
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %d byte(s)\n", sizeof(float));
-        return (0);
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }       
