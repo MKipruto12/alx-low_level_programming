5cc306ee6f5dd4a188a13d98cfa73b892e4d4697
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 10:57:08 -0800

    comments modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/* #include- library header
+/** #include- library header
  * main- starts execution and termination of program
  * return 0: means program was executed properly else
  */
-int main(void) {
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
