a8ffca3948f591acc104e20880c3f7ab9d94ef52
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 11:04:27 -0800

    white space addition

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /*
- * main- starts execution and termination of program
+ * main-starts execution and termination of program printing phrase
  * return 0: means program was executed properly else
  */
 int main(void) 
