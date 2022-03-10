1e2796c48a71158b4010c80ff7b1c803f1109f5b
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-10 10:40:19 -0800

    changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /* library header */
-int main() {
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 	/*puts used in place of printf to output Programming is like building a multilingual puzzle
-	 *\n moves to next line*/
+	 *\n moves to next line
+	 */
 }
