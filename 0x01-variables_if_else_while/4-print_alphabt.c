30f7f2729109840faaa0d04a392cd20507690dde
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 14:04:22 -0800

    swp file deleted

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- code block outputs alphabets in lowercase excluding q and e
+ * Return: 0 if executed else non zero
+ */
+int main(void)
+{
+	char abc;
+	abc = 'a';
+	for (; abc <= 'z'; abc++)
+	{
+		putchar(abc);
+		if (abc != 'e' || abc != 'q')
+		{
+			putchar(abc++);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
