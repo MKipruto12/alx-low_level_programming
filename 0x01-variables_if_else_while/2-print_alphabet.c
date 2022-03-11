d6060cc39f23d06cfc5e47af4af48e81f76185c3
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 12:58:47 -0800

    alphabets in loweercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-starting point, code block outputs alphabet (abc_z) in lowercase
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	char abc_z;
+	abc_z = 'a'
+	for (; abc_z <= 'z';)
+	{
+		putchar(abc_z);
+	}
+	putchar('\n',abc_z++);
+	return (0);
+}
