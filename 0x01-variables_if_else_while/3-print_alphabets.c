08d0df4b83d4593d84ac13d791173a0d241b71e0
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 13:34:34 -0800

    deleted swp file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- code block prints alphabet abC_Z in lowercase then in uppercase)
+ * Return: 0 if executed successfully, else non-zero
+ */
+int main(void)
+{
+	char abcz, ABCZ;
+
+	for (abcz = 'a', ABCZ = 'A'; abcz <= 'z' || ABCZ <= 'Z';)
+	{
+		putchar(abcz++, ABCZ++);
+	}
+	putchar('\n');
+	return (0);
+}	
