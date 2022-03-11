0fbaa1b14e9cd3f8447dd7bb04fc0add36f7ab23
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 15:21:25 -0800

    swp deletion

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- prints possible combos of three digits
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 48;
+	b = 49;
+	c = 50;
+	for (; a < 58; a++)
+		for (; b < 58; b++)
+			for (; c < 58; c++)
+				if (c > b && b > a)
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 && b != 56)
+						putchar(',');
+						putchar(' ');
+	putchar('\n');
+	return (0);
+}
