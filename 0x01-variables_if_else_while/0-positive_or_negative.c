c189d4e9f46c07fc711c7a425e755aac1d70b088
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 11:11:22 -0800

    tab spacing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,22 @@
 #include <stdlib.h>
 
 /**
- * main- starting
+ * main- starting point
+ * executes if else statements
  * Return: 0 if executed
  */
 
 int main(void)
 {
-    int n;
+	int n;
+	srand(time(0);
+	n = rand() - RAND_MAX /2;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n > 0)
-    {
-        printf("%d is positive\n", &n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", &n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", &n);
-    }
-
-    return (0);
-
+	if (n > 0)
+		printf("%d is positive\n", &n);
+	else if (n == 0)
+		printf("%d is zero\n", &n);
+	else if (n < 0)
+		printf("%d is negative\n", &n);
+	return (0);
 }
