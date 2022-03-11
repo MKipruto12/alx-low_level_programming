1eef3e68626462c641072cef2096c9973d66af51
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 14:42:59 -0800

    outputs single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-code block prints single digit numbers of base 10
+ * Return: 0 if executed successfully
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 48;
+	for(; x <58; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
