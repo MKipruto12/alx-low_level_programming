88e925dc415becb454175f98d2bdd6df92d4b356
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 15:09:39 -0800

    modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int num;
 
-	num = 0;
-	for (; num < 10; num++)
+	for (num = 0; num < 10; num++)
+	{
 		putchar((num % 10) + '0');
-		if (num ==9 )
+		if (num ==9)
 			continue;
 		putchar(',');
 		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
