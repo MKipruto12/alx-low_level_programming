908aa98785758d45082fe21d9a17286e5c7cd38c
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-11 15:26:19 -0800

    swp deletion

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,15 +12,25 @@ int main(void)
 	b = 49;
 	c = 50;
 	for (; a < 58; a++)
+	{
 		for (; b < 58; b++)
+		{
 			for (; c < 58; c++)
+			{
 				if (c > b && b > a)
+				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
 					if (a != 55 && b != 56)
+					{
 						putchar(',');
 						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
