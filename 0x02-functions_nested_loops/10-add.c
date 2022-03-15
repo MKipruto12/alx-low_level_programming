fc57d3c5c78b558e03b1a6342082390478655c12
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 16:25:02 -0700

    sum of int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add- adds two ints and returns the result
+ * @a: int one to be added
+ * @b: int two to be added
+ * Return: sum of int a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
