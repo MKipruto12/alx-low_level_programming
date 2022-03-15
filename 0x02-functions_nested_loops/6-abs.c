1e84b20198db2e99cc6d9c4225ab39dc8c1e2dc9
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 15:44:08 -0700

    returens absolute val of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs- computes absolute val of int
+ * @n: int to be checked
+ * Return: absolute val of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
