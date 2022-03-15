b2eef2c98760ea27b86674d7ceda80ebbc545a0e
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 15:39:35 -0700

    prints sign of int

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,9 @@
 /**
  * print_sign- prints sign of num
  * @n: int to be checked
- * Return: 1 and prints + if n is greater than 0, returns 0 and prints 0 if n is zero, returns -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than 0,
+ * 0 and prints 0 if n is zero,
+ * -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
@@ -16,7 +18,7 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	else
 	{
