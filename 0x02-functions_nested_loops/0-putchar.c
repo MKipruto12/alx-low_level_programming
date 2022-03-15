0e57d46b0bddaa63fd98883d12fc0467218cf4fc
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 14:15:00 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main- code block prints _putchar followed by new line
+ * Return: 0 if successfull
+ */
+int main(void)
+{
+	char ch = "_putchar";
+
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
