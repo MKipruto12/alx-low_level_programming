fcae8caac74f92ec4bc57cc104f57b24f0e33567
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 15:08:16 -0700

    printin alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- code block prints 10 times the alphabet in lowecase
+ */
+
+void print_alphabet_x10(void)
+{
+	char abc_z;
+	int i = 0;
+
+	for(; i<10;)
+	{
+		abc_z = 'a';
+		for(; abc_z <= 'z';)
+		{
+			_putchar(abc_z++);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
