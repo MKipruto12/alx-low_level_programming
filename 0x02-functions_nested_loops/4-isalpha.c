117087956fc598ada8829a20597856e9a6ee79c0
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 15:29:40 -0700

    checks for alphabetic char

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _isalpha:code block checks for alphabetic char
+/**
+ * _isalpha- code block checks for alphabetic char
  * @c: char to be checked
  * Return: 1 is c is a letter else o
  */
