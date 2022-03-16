9e0b1e00c42ca7ce6e7b1751787818b2a6ba70c7
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 17:42:49 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,10 +11,10 @@ int main(void)
 	unsigned long int j, k, next, sum;
 
 	j = 1;
-	k =2;
+	k = 2;
 	sum = 0;
 
-	for (i = 1; i <+ 33; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
@@ -26,4 +26,4 @@ int main(void)
 	}
 	printf("%lu\n", sum);
 	return (0);
-}	
+}
