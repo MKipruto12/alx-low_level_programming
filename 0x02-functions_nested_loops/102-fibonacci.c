42ea5f7e27b262843ca12cfa54d9c6c0e0e919f5
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-15 17:30:50 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+iinclude <stdio.h>
+
+/**
+ * main- prints first fibonacchi sequence
+ * Return: 0 if successfully executed
+ */
+
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <=50; ++i)
+	{
+		if (j != 2036501174)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+}	
