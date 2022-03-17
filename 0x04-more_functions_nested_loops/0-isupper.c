8ad6bd31c25b341e403c8e71a40079ffb9153c8e
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-17 07:16:18 -0700

    modified

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,17 @@
 /**
  * _isupper - checks uppercase character
  * @c: character to be checked
- * Return: 1 if uppercase
- * Return: 0 if lowercase
+ * Return: 1 if uppercase, Return: 0 if lowercase
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
