9e3e3a0f4e0bcaf6481b4649c2c0810380bccebf
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-21 14:51:29 -0700

    prints char of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
 
 void puts2(char *str)
 {
-	int count;
+	int count = 0;
 
-	count = 0;
-	for (; str[count] != '\0'; count++)
+	while (count >= 0)
 	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
 		if (count % 2 == 0)
 			_putchar(str[count]);
 		count++;
