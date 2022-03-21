b4aa8793aa4786002d0588978fe6523137b0531c
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-21 14:23:56 -0700

    reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string- reverses string
+ * @s: string to be checked
+ */
+
+void rev_string(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	count = 0;
+	for (; s[count] != '\0';)
+		counti++;
+	str = s;
+
+	i = 0;
+	for (; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+	}
+}
+
