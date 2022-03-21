8f1e0e32510229b4835756fd979ee155896d9a69
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-21 14:03:43 -0700

    prints in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev- prints string in reverse
+ * @s: string to be checked
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	for (; s[i] != '\0';)
+		i++;
+	for (i--; i = 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
