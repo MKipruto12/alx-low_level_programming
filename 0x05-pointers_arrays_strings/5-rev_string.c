4c1f081ee56f5ddd6e43e7b6e0762ffca92f24ad
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-21 14:28:00 -0700

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,16 +12,18 @@ void rev_string(char *s)
 
 	count = 0;
 	for (; s[count] != '\0';)
-		counti++;
+		count++;
 	str = s;
 
 	i = 0;
 	for (; i < (count - 1); i++)
 	{
 		for (j = i + 1; j > 0; j--)
+		{
 			temp = *(str + j);
 			*(str + j) = *(str + (j - 1));
 			*(str + (j - 1)) = temp;
+		}
 	}
 }
 
