886cac9e5cf9242f4d41382d345b616f86c0d67d
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-24 12:07:55 -0700

    modified mmain.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,22 +5,23 @@
  * @src: source.
  * Return: the pointer to dest.
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
+	while (dest[count] != '\0')
 	{
+		count++;
+	}
+	while (src[count2] != '\0')
+	{
+		dest[count] = src[count2];
+		count2++;
 		count++;
 	}
 
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	dest[count] = '\0';
+
 	return (dest);
 }
