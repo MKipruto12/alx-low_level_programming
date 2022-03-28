0cd6ccc578bdc1d171de042f338aecb4279d3e77
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-28 13:30:33 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,8 @@
  * @n: bytes to be copied
  * Return: a pointer to dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
