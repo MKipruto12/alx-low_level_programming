c3e7d2aa7d89233f0c9be7c956ce10e7a7aa33bd
MKipruto12 <mercykipruto2017@gmail.com>
2022-03-28 13:28:53 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
-	i = 0;
-
-	for (; i < n;)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	return (dest);
 }
