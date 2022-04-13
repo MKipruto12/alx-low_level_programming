299ab066c4e185dcc8f193b63704f1ee90f9b892
MKipruto12 <mercykipruto2017@gmail.com>
2022-04-13 10:09:45 -0700

    iterate over array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * array_iterator - iterates over an array
+ * @array: array to iterate
+ * @size: number of elements of array
+ * @action: funct to perfom on array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int param))
+{
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+	for(i = 0; i < size;)
+	{
+		action(array[i]);
+		i++;
+	}
+}
