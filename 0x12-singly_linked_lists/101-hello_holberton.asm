3c64d2c771eb536e9c5dd913e5098d35f465f8df
MKipruto12 <mercykipruto2017@gmail.com>
2022-04-21 11:40:04 -0700

    64-bit asembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,18 @@
-       global    main
-          extern    printf
+	extern printf
+
+	section .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	section .text
+
+	global main
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,i0
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
